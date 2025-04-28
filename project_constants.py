17637050bab5f94334f2f3b29a2f99d4c434bbe6
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2025-04-28 23:00:37 +0300

    Integrates a simple RAG mechanism in the application

---
+++ project_constants.py
@@ -0,0 +1,4 @@
+DATABASE_PATH = "./db/my_database.db"
+LM_STUDIO_URL = "http://localhost:1234/v1"
+EMBEDDING_MODEL_NAME = "text-embedding-nomic-embed-text-v1.5"
+LLM_MODEL_NAME = "google/gemma-3-1b-it"
