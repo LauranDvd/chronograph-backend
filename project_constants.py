71be64acb2b9990623e55c99f17dc6d5c30f7166
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2025-05-13 00:06:06 +0300

    Adds a constitutional model to the architecture

--- project_constants.py
+++ project_constants.py
@@ -1,4 +1,6 @@
 DATABASE_PATH = "./db/my_database.db"
 LM_STUDIO_URL = "http://localhost:1234/v1"
 EMBEDDING_MODEL_NAME = "text-embedding-nomic-embed-text-v1.5"
-LLM_MODEL_NAME = "google/gemma-3-1b-it"
+LLM_MODEL_NAME = "google/gemma-3-4b-it"
+CONSTITUTIONAL_MODEL_NAME = "google/gemma-3-4b-it"
+# NOTE: the constitutional model does not behave properly if set to gemma-3-1b-it
